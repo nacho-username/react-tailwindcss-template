e1df9cd1d1f0f512bf2595364a9a4658d9d974ac
Murray Williams <murray@waterfalldigital.co.za>
2022-06-15 14:12:20 +0200

    Clean up unused helper function

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,8 +1,3 @@
 export function roundToTwo(num) {
   return +(Math.round(num + 'e+2') + 'e-2')
 }
-
-export function round(num) {
-  var m = Number((Math.abs(num) * 100).toPrecision(20))
-  return (Math.round(m) / 100) * Math.sign(num)
-}

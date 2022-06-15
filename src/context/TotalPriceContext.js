cfd7772bf861f2dac4ea9cc30846f673173eada7
Murray Williams <murray@waterfalldigital.co.za>
2022-06-15 09:45:12 +0200

    Adds roomPricing to context state

--- src/context/TotalPriceContext.js
+++ src/context/TotalPriceContext.js
@@ -9,6 +9,7 @@ export const PricingProvider = ({ children }) => {
     plan: 'monthly',
     numRooms: 1,
     totalPrice: '19.95',
+    roomPricing: roomPricing,
   })
 
   const updatePricePackage = (updPackage) => {
@@ -18,41 +19,6 @@ export const PricingProvider = ({ children }) => {
     }
     setPricePackage(newPricePackage)
   }
-
-  // const updateRoomCount = (updRoomCount) => {
-  //   const updPrice = {
-  //     ...pricePackage,
-  //     numRooms: updRoomCount,
-  //   }
-
-  //   setPricePackage(updPrice)
-  //   updateTotalPrice(updRoomCount)
-  // }
-
-  // const updateTotalPrice = (roomCount) => {
-  //   const newTotalPrice = parseFloat(roomPricing[roomCount - 1].price).toFixed(
-  //     2
-  //   )
-  //   if (roomCount <= 40) {
-  //     const updTotalPrice = {
-  //       ...pricePackage,
-  //       totalPrice: newTotalPrice,
-  //     }
-
-  //     setPricePackage(updTotalPrice)
-  //   }
-  // }
-
-  // const updatePlan = (plan) => {
-  //   const updPrice = {
-  //     ...pricePackage,
-  //     plan: plan,
-  //   }
-
-  //   setPricePackage(updPrice)
-  //   console.log(pricePackage)
-  //   updateTotalPrice(pricePackage.numRooms)
-  // }
 
   return (
     <TotalPriceContext.Provider value={{ pricePackage, updatePricePackage }}>

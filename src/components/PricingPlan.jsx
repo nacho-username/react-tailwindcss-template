5ef1359ffd1638bc7eafbe5ab37e7d69a4cec5fd
Murray Williams <murray@waterfalldigital.co.za>
2022-06-15 14:56:53 +0200

    Refactors Pricing plan component

--- src/components/PricingPlan.jsx
+++ src/components/PricingPlan.jsx
@@ -4,20 +4,24 @@ import { roundToTwo } from '../utilities/helpers'
 
 function PricingPlan() {
   const { pricePackage } = useContext(TotalPriceContext)
-  const [totalPrice, setTotalPrice] = useState(pricePackage.totalPrice)
+  const [totalPrice, setTotalPrice] = useState(
+    pricePackage.roomPricing[pricePackage.numRooms - 1].price
+  )
 
   useEffect(() => {
+    const currentTotalPrice =
+      pricePackage.roomPricing[pricePackage.numRooms - 1].price
+
     if (pricePackage.plan === 'annually') {
-      const annualPrice = roundToTwo(pricePackage.totalPrice * 12)
-      setTotalPrice(annualPrice)
+      setTotalPrice((currentTotalPrice * 12).toFixed(2))
     } else {
-      setTotalPrice(pricePackage.totalPrice)
+      setTotalPrice(currentTotalPrice)
     }
   }, [pricePackage, totalPrice])
 
   return (
-    <div className='flex gap-12 mt-8'>
-      <div className='flex-1 rounded-3xl shadow-lg p-8'>
+    <div className='flex gap-24 mt-8 justify-center'>
+      <div className='basis-96 rounded-xl shadow-lg p-8'>
         <div className='rounded-full bg-gray-200 uppercase text-primary text-xs px-3 py-1 inline'>
           14 day free trial
         </div>
@@ -30,25 +34,59 @@ function PricingPlan() {
             /{pricePackage.plan.toUpperCase()}
           </span>
         </div>
-        <div className='text-xs text-blue mt-4 font-light'>
+        <div className='text-2xs text-blue mt-4 font-light'>
           A 14 day free trial grants you access to ALL of our direct bookings
           PMS features. No credit card details required for sign up!
         </div>
         <div className='text-sm text-bold text-blue mt-4 font-normal'>
           Features Tailored to BnB / Guesthouse
         </div>
+        <div className='text-sm'>
+          <ul class='w-full rounded-lg mt-2 mb-3 text-blue-800'>
+            <li class='flex my-2'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-5 w-5'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
+                  clipRule='evenodd'
+                />
+              </svg>
+              Third-party channel connections
+            </li>
+            <li class='flex my-2'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-5 w-5'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
+                  clipRule='evenodd'
+                />
+              </svg>
+              Unrivalled customer support
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className='flex-1 bg-gradient-to-r from-primary to-rose-400 rounded-3xl p-8 !text-white rounded-3xl shadow-lg'>
+      <div className='basis-96 bg-gradient-to-r from-primary to-rose-400 p-8 rounded-xl !text-white shadow-lg'>
         <div className='rounded-full bg-gray-200 uppercase text-primary text-xs px-3 py-1 inline'>
           Inn Style Member
         </div>
         <div id='monthlyPrice' className='text-4xl font-Lato font-bold mt-4'>
-          £ {totalPrice.toFixed(2)}
+          £ {totalPrice}
           <span className='text-xs uppercase font-light ml-2'>
             /{pricePackage.plan.toUpperCase()}
           </span>
         </div>
-        <div className='text-xs text-white mt-4 font-light'>
+        <div className='text-2xs text-white mt-4 font-light'>
           A 14 day free trial grants you access to ALL of our direct bookings
           PMS features. No credit card details required for sign up!
         </div>

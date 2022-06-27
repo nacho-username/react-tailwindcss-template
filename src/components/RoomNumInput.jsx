18745da83eba2e7adf3e61faaf62989bc4b66bae
Murray Williams <murray@waterfalldigital.co.za>
2022-06-27 13:00:33 +0200

    Adds range slider

--- src/components/RoomNumInput.jsx
+++ src/components/RoomNumInput.jsx
@@ -13,73 +13,39 @@ function RoomNumInput() {
 
   const handleChange = (e) => {
     setRoomNum(e.target.value)
-    updatePricePackage({
-      numRooms: e.target.value,
-    })
-  }
-
-  const handlePlus = () => {
-    if (pricePackage.type !== 'campsite') {
-      setUnit('rooms')
-      if (roomNum <= 39) {
-        setRoomNum(roomNum + 1)
-        updatePricePackage({
-          numRooms: roomNum + 1,
-        })
-      }
-    } else {
-      if (roomNum <= 99) {
-        setUnit('pitchs')
-        setRoomNum(roomNum + 1)
-        let getCampsitePricing = calculateCampsitePricing(roomNum)
-        updatePricePackage({
-          totalPrice: parseFloat(getCampsitePricing),
-          numRooms: roomNum + 1,
-        })
-      }
-    }
-  }
-
-  const handleMinus = () => {
-    if (pricePackage.type !== 'campsite') {
-      if (roomNum > 1) {
-        setRoomNum(roomNum - 1)
-        updatePricePackage({
-          numRooms: roomNum - 1,
-        })
-      } else if (roomNum === 1) {
-        setUnit('room')
-      }
-    } else {
-      if (roomNum > 1) {
-        setUnit('pitchs')
-        setRoomNum(roomNum - 1)
-        let getCampsitePricing = calculateCampsitePricing(roomNum)
-        updatePricePackage({
-          totalPrice: parseFloat(getCampsitePricing),
-          numRooms: roomNum - 1,
-        })
-      }
+    if (roomNum > 0) {
+      updatePricePackage({
+        numRooms: e.target.value,
+      })
     }
   }
 
   return (
-    <div className='flex justify-center'>
-      <label className='label'>
+    <div className='w-2/3 mx-auto'>
+      <label className='label w-32 mx-auto'>
         <span className='label-text mr-2 text-lightgray'>I have</span>
-        <div className='flex align-middle justify-center w-[130px] py-3 px-6 border text-primary border-lightgray rounded-full'>
-          <div onClick={handleMinus} className='text-md ml-2 cursor-pointer'>
-            -
-          </div>
-          <div onChange={handleChange} className='text-sm px-4 '>
-            {roomNum}
-          </div>
-          <div onClick={handlePlus} className='text-md mr-2 cursor-pointer'>
-            +
-          </div>
+        <span className='label-text mr-2 text-primary'>{roomNum}</span>
+        <div className='label-text ml-2 text-lightgray'>
+          {roomNum > 1 ? 'rooms' : 'room'}
         </div>
-        <span className='label-text ml-2 text-lightgray'>{unit}</span>
       </label>
+      <div className='text-primary grow  rounded-full'>
+        {/* <input
+            type='number'
+            value={roomNum}
+            onChange={handleChange}
+            className='text-sm py-4 w-24'
+          /> */}
+        <input
+          type='range'
+          min='1'
+          max='40'
+          value={roomNum}
+          onChange={handleChange}
+          step='1'
+          className='range range-primary'
+        />
+      </div>
     </div>
   )
 }
